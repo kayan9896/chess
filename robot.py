33b5a034fbcd7233e91d05f364b624ca2214108e
kayan9896 <90473306+kayan9896@users.noreply.github.com>
2024-02-20 19:41:17 +0000

    eval

--- robot.py
+++ robot.py
@@ -1,4 +1,11 @@
 import random
+from piece import Piece
+from king import King
+from queen import Queen
+from rook import Rook
+from bishop import Bishop
+from knight import Knight
+from pawn import Pawn
 
 class Robot:
     def __init__(self, side):
@@ -19,3 +26,29 @@ class Robot:
             return random.choice(valid_moves)
         else:
             return None  # No valid moves (e.g., checkmate)
+
+
+    def eval(self, board):
+        score = 0
+        for i in range(8):
+            for j in range(8):
+                piece = board[i][j]
+                if piece:
+                    value = self.get_piece_value(piece)
+                    if piece.side == self.side:
+                        score += value
+                    else:
+                        score -= value
+        return score
+
+    def get_piece_value(self, piece):
+        if isinstance(piece, Pawn):
+            return 1
+        elif isinstance(piece, Knight) or isinstance(piece, Bishop):
+            return 3
+        elif isinstance(piece, Rook):
+            return 5
+        elif isinstance(piece, Queen):
+            return 9
+        elif isinstance(piece, King):
+            return 1000  # High value to prioritize king safety
