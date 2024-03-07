dd9818ee29048cd1714ac7e085fcb9832e430712
kayan9896 <90473306+kayan9896@users.noreply.github.com>
2024-03-07 10:35:30 +0000

    ux

--- lib/app.py
+++ lib/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify
 import chess
 from ai import AI  # Assuming the AI class is implemented in a separate file named "ai.py"
 
+from flask_cors import CORS  # Import the CORS module
 app = Flask(__name__)
+CORS(app) 
 game = None
 ai = AI()
 
@@ -13,6 +15,7 @@ def to2d(game):
         l.strip('\"')
         rt.append(l.split(' '))
     return rt
+
 @app.route('/start', methods=['GET'])
 def start_game():
     global game
@@ -31,15 +34,20 @@ def make_move():
         return jsonify({'message': 'Move parameter is missing.'}), 400
 
     try:
+        move.replace('-','')
         game.push_san(move)
     except ValueError:
         return jsonify({'message': 'Invalid move.'}), 400
+    gg="Gameover" if game.is_game_over else ""
+    return jsonify({'board': to2d(game), "message":move + gg }), 200
 
+@app.route('/getmove', methods=['GET'])
+def get_move():
     if not game.is_game_over() and game.turn == chess.BLACK:
-        best_move, _ = ai.minimax(game, 3, float('-inf'), float('inf'), True)
+        best_move, _ = ai.minimax(game, 5, float('-inf'), float('inf'), True)
         game.push(best_move)
-
-    return jsonify({'board': game.to2d(game)}), 200
+    gg="Gameover" if game.is_game_over else ""
+    return jsonify({'board': to2d(game), "message":best_move.__str__()+gg}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
